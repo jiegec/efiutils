aba189e2940a868d9485bed1e96dd29d3cad6598
jiegec <noc@jiegec.ac.cn>
2021-05-02 17:28:31 +0800

    Dump to stdout instead

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 set -e
 cargo build
 mkdir -p esp/EFI/Boot
-cp target/x86_64-unknown-uefi/debug/efivars.efi esp/EFI/Boot/BootX64.efi
+cp target/x86_64-unknown-uefi/debug/efivars.efi esp/
 qemu-system-x86_64 -bios /usr/share/edk2-ovmf/x64/OVMF.fd -drive format=raw,file=fat:rw:esp -nographic
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 #![feature(abi_efiapi, negative_impls)]
 
-use core::borrow::Borrow;
+use core::{borrow::Borrow, fmt::Write};
 
 use log::*;
 use uefi::{prelude::*, proto::media::file::RegularFile, Char16, Guid, Identify};
@@ -41,7 +41,6 @@ pub struct HiiConfigRouting {
 #[entry]
 fn efi_main(image: uefi::Handle, st: SystemTable<Boot>) -> Status {
     uefi_services::init(&st).expect_success("failed to initialize utilities");
-    info!("Hello, world!");
     let bt = st.boot_services();
 
     let routing = bt.locate_protocol::<HiiConfigRouting>().unwrap().unwrap();
@@ -49,40 +48,8 @@ fn efi_main(image: uefi::Handle, st: SystemTable<Boot>) -> Status {
 
     let mut results: *const Char16 = 0 as *const Char16;
     let res = (routing.export_config)(&routing, &mut results);
-    info!("Got res {:?}", res);
     let s = unsafe { CStr16::from_ptr(results) };
+    st.stdout().output_string(s).unwrap().unwrap();
 
-    let sfs = bt.locate_protocol::<SimpleFileSystem>().unwrap().unwrap();
-    let sfs = unsafe { &mut *sfs.get() };
-    let mut directory = sfs.open_volume().unwrap().unwrap();
-    let handle = directory
-        .open(
-            "hii",
-            proto::media::file::FileMode::CreateReadWrite,
-            FileAttribute::from_bits_truncate(0),
-        )
-        .unwrap()
-        .unwrap();
-    let mut file = unsafe { RegularFile::new(handle) };
-    let slice = s.to_u16_slice();
-    file.write(unsafe {
-        core::slice::from_raw_parts(slice.as_ptr() as *const u8, slice.len() * 2)
-    })
-    .unwrap()
-    .unwrap();
-    info!("Written to file hii");
-
-    info!("Enter any key to exit");
-    loop {
-        let events = &mut [st.stdin().wait_for_key_event()];
-        &st.boot_services().wait_for_event(events).unwrap();
-        match st.stdin().read_key().unwrap().unwrap() {
-            Some(key) => {
-                info!("Got key {:?}", key);
-                break;
-            }
-            None => {}
-        };
-    }
     Status::SUCCESS
 }
